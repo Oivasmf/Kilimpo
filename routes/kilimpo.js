c6b1757773d5ab5a9bf16b9edcbb5a8c218bbb74
Fernando Jacques Peiretti <fernandopeiretti@users.noreply.github.com>
2021-04-24 19:02:22 -0300

    Update kilimpo.js

--- routes/kilimpo.js
+++ routes/kilimpo.js
@@ -1,17 +1,15 @@
 const router = require('express').Router();
 const { check, body, validationResult } = require('express-validator');
-//PERGUNTAR .bail(), .withmessage(), conexão com back-end, como desmarcar checkbox, retorno
 router.post('/', [
     check('nome','Nome é campo obrigatório.').trim().escape().notEmpty(),
-    check('telefone').trim().escape(),
-    check('whatsapp').trim().escape().notEmpty().withMessage('Você deve marcar se possui Whatsapp'),
+    check('telefone').trim().escape().optional(),
+    check('whatsapp').notEmpty().withMessage('Você deve marcar se possui Whatsapp'),
     check('marca','Marca é um campo obrigatório.').trim().escape().notEmpty(),
     check('modelo','Modelo é um campo obrigatório.').trim().escape().notEmpty(),
     check('ano').trim().escape().optional(),
     check('placa', 'Placa é um campo obrigatório.').trim().escape().notEmpty(),
     check('data', 'Selecione uma data válida.').notEmpty().custom(value => {
-        var data = new Date(value);
-        console.log(data.getDay());
+        var data = new Date(value)
         if(data.getDay()>0 && data.getDay()<6){
             return true;
         } else return false;
