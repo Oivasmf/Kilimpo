24c1549a0cf00cb1951161ed0957ebbc9ab902b7
Fernando Jacques Peiretti <fernandopeiretti@users.noreply.github.com>
2021-04-26 23:02:36 -0300

    Update kilimpo.js

--- routes/kilimpo.js
+++ routes/kilimpo.js
@@ -11,7 +11,7 @@ router.post('/', [
     check('data').custom(value => {
         var data = new Date(value);
         var data_atual = new Date();
-        data.setUTCHours(23, 59);
+        data.setHours(23, 59);
 
         data_atual.setTime(data_atual.getTime() + data_atual.getTimezoneOffset());
         console.log(data.getDay()>0 && data.getDay()<6);
